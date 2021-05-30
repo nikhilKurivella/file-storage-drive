46a954fb0957244538a89b3fd7f8284da230cb4c
Nikhil Sai Kurivella <nikhilsaikurivella@Nikhils-MacBook-Pro.local>
2021-05-30 15:52:18 -0600

    Note creation

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,15 +1,16 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import com.udacity.jwdnd.course1.cloudstorage.model.NoteForm;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.EncryptionService;
+import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/home")
@@ -17,23 +18,68 @@ public class HomeController {
 
     private final UserService userService;
     private final NoteService noteService;
+    private final EncryptionService encryptionService;
 
     public HomeController(
-            UserService userService, NoteService noteService) {
+            FileService fileService, UserService userService, NoteService noteService, EncryptionService encryptionService) {
         this.userService = userService;
         this.noteService = noteService;
+        this.encryptionService = encryptionService;
     }
 
     @GetMapping
     public String getHomePage(
             Authentication authentication, @ModelAttribute("newNote") NoteForm newNote, Model model) {
         Integer userId = getUserId(authentication);
+        model.addAttribute("notes", noteService.getNoteListings(userId));
+        model.addAttribute("encryptionService", encryptionService);
 
         return "home";
     }
+
+    @PostMapping("/add-note")
+    public String newNote(Authentication authentication, @ModelAttribute("newNote") NoteForm newNote, Model model){
+        String userName = authentication.getName();
+        String newTitle = newNote.getTitle();
+        String noteIdStr = newNote.getNoteId();
+        String newDescription = newNote.getDescription();
+        System.out.println(userName+ " " +" " + newDescription);
+
+        if (noteIdStr.isEmpty()){
+            noteService.addNote(newTitle, newDescription, userName);
+        }
+        else{
+            Note existingNote = noteService.getNote(Integer.parseInt(noteIdStr));
+            noteService.updateNote(existingNote.getNoteId(), newTitle, newDescription);
+        }
+        Integer userId = getUserId(authentication);
+        model.addAttribute("notes", noteService.getNoteListings(userId));
+        model.addAttribute("result", "success");
+        return "result";
+    }
+
+
     private Integer getUserId(Authentication authentication) {
         String userName = authentication.getName();
         User user = userService.getUser(userName);
         return user.getUserid();
     }
+
+
+    @GetMapping(value = "/get-note/{noteId}")
+    public Note getNote(@PathVariable Integer noteId) {
+        return noteService.getNote(noteId);
+    }
+
+    @GetMapping(value = "/delete-note/{noteId}")
+    public String deleteNote(Authentication authentication, @PathVariable Integer noteId,
+                             @ModelAttribute("newNote") NoteForm newNote, Model model){
+        System.out.println("I'm here to delete the note you want");
+        noteService.deleteNote(noteId);
+        Integer userId = getUserId(authentication);
+        model.addAttribute("result", "success");
+        return "result";
+    }
+
 }
+
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Note.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Note.java
@@ -1,47 +1,52 @@
 package com.udacity.jwdnd.course1.cloudstorage.model;
 
 public class Note {
-    private Integer noteid;
-    private String notetitle;
-    private String notedescription;
-    private Integer userid;
+    private Integer noteId;
+    private String noteTitle;
+    private String noteDescription;
+    private Integer userId;
 
-    public Note(Integer noteid, String notetitle, String notedescription, Integer userid) {
-        this.noteid = noteid;
-        this.notetitle = notetitle;
-        this.notedescription = notedescription;
-        this.userid = userid;
+    public Note(Integer noteId, String noteTitle, String noteDescription, Integer userId) {
+        this.noteId = noteId;
+        this.noteTitle = noteTitle;
+        this.noteDescription = noteDescription;
+        this.userId = userId;
     }
 
-    public Integer getNoteid() {
-        return noteid;
+    public Note(String noteTitle, String noteDescription) {
+        this.noteTitle = noteTitle;
+        this.noteDescription = noteDescription;
     }
 
-    public void setNoteid(Integer noteid) {
-        this.noteid = noteid;
+    public Integer getNoteId() {
+        return noteId;
     }
 
-    public String getNotetitle() {
-        return notetitle;
+    public void setNoteId(Integer noteId) {
+        this.noteId = noteId;
     }
 
-    public void setNotetitle(String notetitle) {
-        this.notetitle = notetitle;
+    public String getNoteTitle() {
+        return noteTitle;
     }
 
-    public String getNotedescription() {
-        return notedescription;
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
     }
 
-    public void setNotedescription(String notedescription) {
-        this.notedescription = notedescription;
+    public String getNoteDescription() {
+        return noteDescription;
     }
 
-    public Integer getUserid() {
-        return userid;
+    public void setNoteDescription(String noteDescription) {
+        this.noteDescription = noteDescription;
     }
 
-    public void setUserid(Integer userid) {
-        this.userid = userid;
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
     }
 }
